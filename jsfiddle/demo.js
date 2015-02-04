875abbee1b853cc8dc127a7b15fe30ef9e63cfcb
Sanjay Chouksey <sanjayc77@users.noreply.github.com>
2015-02-04 12:07:55 -0800

    Update demo.js

--- jsfiddle/demo.js
+++ jsfiddle/demo.js
@@ -1,1 +1,7 @@
-console.log('Hello world');
+var Hello = React.createClass({
+    render: function() {
+        return <div>Hello {this.props.name}</div>;
+    }
+});
+ 
+React.render(<Hello name="World" />, document.body);
